656b58f8bfc7bf5f1d06ffce69a3e37fb9ae552b
Anne Jan Brouwer <ajbrouwer@totalactivemedia.nl>
2014-10-21 20:46:18 +0200

    Use VCC, GND, XTAL pins and PD5 for display

--- MD_MAX72xx_Message.ino
+++ MD_MAX72xx_Message.ino
@@ -19,9 +19,9 @@
 // need to be adapted
 #define	MAX_DEVICES	1
 
-#define	CLK_PIN		13  // or SCK
-#define	DATA_PIN	11  // or MOSI
-#define	CS_PIN		10  // or SS
+#define	CLK_PIN		5   // or SCK
+#define	DATA_PIN	20  // or MOSI
+#define	CS_PIN		21  // or SS
 
 // SPI hardware interface
 MD_MAX72XX mx = MD_MAX72XX(CS_PIN, MAX_DEVICES);
