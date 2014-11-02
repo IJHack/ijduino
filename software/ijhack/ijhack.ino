d6c9ad236b3ec83bc3c04de0f7c3b8a1f620a3f6
realitygaps <realitygaps@gapsinreality.com>
2014-11-02 03:47:41 +0100

    Adding arrows animation

--- software/ijhack/ijhack.ino
+++ software/ijhack/ijhack.ino
@@ -1,30 +1,14 @@
-
-/*                            IJduino 1.0
-Requires:
-* 8X8 LED grid MAX7219
-* Mini Breadboard
-* Atmega328
-* Button
-* 2 Wires
-* 4.5 Volt
-                        (c) IJhack 2014 GNU GPL 
-                           http://ijhack.org/
-*/
 #include "LedControl.h"
 
-/* using VCC, GND, DIN 21, CS 21, CLK 5 for the MAX7219 */
 LedControl lc=LedControl(20,5,21,1);
 
-int lowPin = 11;             /* ground pin for the buton ;-) */
-int buttonPin = 9;           /* choose the input pin for the pushbutton */
+int animations = 12;
 
-int animations = 11;         /* the number of animations we have */
+int lowPin = 11;
+int buttonPin = 9;               // choose the input pin for the pushbutton
 
-int lastButtonState = LOW;   /* the previous reading from the input pin */
-long lastDebounceTime = 0;   /* the last time the output pin was toggled */
-long debounceDelay = 50;     /* the debounce time; increase if the output flickers */
 int anicount = 0;
-int buttonState = LOW;
+int buttonState = 0;
 
 void setup() {
   /*
@@ -43,6 +27,23 @@ void setup() {
 }
 
 void loop() {
+   // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  // if it is, the buttonState is HIGH:
+  if (buttonState == LOW) {
+    anicount++;
+    if (anicount > animations) {
+      anicount = 0;
+    }
+    anicounting();
+  } else {
+    anicounting();
+  }
+}
+
+void anicounting() {
   switch (anicount) {
     case 0:
       ijhacklogo();
@@ -80,10 +81,13 @@ void loop() {
     case 11:
       snake();
       break;
+    case 12:
+      arrows();
+      break;
   }
 }
 
-bool render(byte* animation, int delaytime) {
+void render(byte* animation, int delaytime) {
   lc.setColumn(0,0,animation[7]);
   lc.setColumn(0,1,animation[6]);
   lc.setColumn(0,2,animation[5]);
@@ -92,47 +96,7 @@ bool render(byte* animation, int delaytime) {
   lc.setColumn(0,5,animation[2]);
   lc.setColumn(0,6,animation[1]);
   lc.setColumn(0,7,animation[0]);
-  
-  long startTime = millis();
-  while ((startTime + delaytime) > millis()){
-  
-    // read the state of the switch into a local variable:
-    int reading = digitalRead(buttonPin);
-
-    // check to see if you just pressed the button 
-    // (i.e. the input went from HIGH to LOW),  and you've waited 
-    // long enough since the last press to ignore any noise:  
-  
-    // If the switch changed, due to noise or pressing:
-    if (reading != lastButtonState) {
-      // reset the debouncing timer
-      lastDebounceTime = millis();
-    } 
-    
-    if ((millis() - lastDebounceTime) > debounceDelay) {
-      // whatever the reading is at, it's been there for longer
-      // than the debounce delay, so take it as the actual current state:
-  
-      // if the button state has changed:
-      if (reading != buttonState) {
-        buttonState = reading;
-  
-        // only toggle the animation if the new button state is HIGH
-        if (buttonState == LOW) {
-          anicount++;
-          if (anicount > animations) {
-            anicount = 0;
-          }
-          return true;
-        }
-      }
-    }
-  
-    // save the reading.  Next time through the loop,
-    // it'll be the lastButtonState:
-    lastButtonState = reading;
-  }
-  return false;  /* don't skip the rest */
+  delay(delaytime);
 } 
 
 void invade() {
@@ -156,10 +120,7 @@ B01111110,
 B00100100,
 B00100100};
   
-  bool skip = render(invade, 500);
-  if (skip) {
-    return;
-  }
+  render(invade, 500);
   render(devade, 500);
 }
 
@@ -184,10 +145,7 @@ B00100100,
 B01011010,
 B10100101};
 
-  bool skip = render(invadesquid, 500);
-  if (skip) {
-    return;
-  }
+  render(invadesquid, 500);
   render(devadesquid, 500);  
 }
 
@@ -283,6 +241,7 @@ B01100011,
 B01111111,
 B01010101};
 
+
 byte ghost9a[8]={
 B00011100,
 B00111110,
@@ -323,6 +282,7 @@ B01100011,
 B01111111,
 B01010101};
 
+
 byte ghost9e[8]={
 B00011100,
 B00111110,
@@ -332,6 +292,7 @@ B01111111,
 B01100011,
 B01111111,
 B01010101};
+
 
 byte ghost10[8]={
 B00001110,
@@ -404,131 +365,37 @@ B00000000,
 B00000000,
 B00000000};
 
-  bool skip;
-  skip = render(ghost1, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost2, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(ghost3, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost4, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost5, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost6, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost7, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost8, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9a, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9b, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9c, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9d, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9e, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9a, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9b, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9c, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9d, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9e, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9a, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9b, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9c, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9d, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9e, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost9, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost10, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost11, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost12, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost13, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost14, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(ghost15, 200);
-  if (skip) {
-    return;
-  }
+  render(ghost1, 200);
+  render(ghost2, 200);  
+  render(ghost3, 200);
+  render(ghost4, 200);
+  render(ghost5, 200);
+  render(ghost6, 200);
+  render(ghost7, 200);
+  render(ghost8, 200);
+  render(ghost9, 200);
+  render(ghost9a, 200);
+  render(ghost9b, 200);
+  render(ghost9c, 200);
+  render(ghost9d, 200);
+  render(ghost9e, 200);
+  render(ghost9a, 200);
+  render(ghost9b, 200);
+  render(ghost9c, 200);
+  render(ghost9d, 200);
+  render(ghost9e, 200);
+  render(ghost9a, 200);
+  render(ghost9b, 200);
+  render(ghost9c, 200);
+  render(ghost9d, 200);
+  render(ghost9e, 200);
+  render(ghost9, 200);
+  render(ghost10, 200);
+  render(ghost11, 200);
+  render(ghost12, 200);
+  render(ghost13, 200);
+  render(ghost14, 200);
+  render(ghost15, 200);
   render(ghost16, 200);
 }
 
@@ -553,6 +420,7 @@ B11100111,
 B01111110,
 B00111100};
 
+
    byte logowink2[8]={  
 B11000000,
 B11000000,
@@ -573,29 +441,14 @@ B00000000,
 B00000000,
 B00000000};
 
-  bool skip;
-  skip = render(logowink0, 500);
-  if (skip) {
-    return;
-  }
-  skip = render(logowink1, 500);
-  if (skip) {
-    return;
-  }
-  skip = render(logowink0, 500);
-  if (skip) {
-    return;
-  }  
-  skip = render(logowink2, 500);
-  if (skip) {
-    return;
-  }
-  skip = render(logowink0, 500);
-  if (skip) {
-    return;
-  }
-  skip = render(logowink3, 500);  
+  render(logowink0, 500);
+  render(logowink1, 500);
+  render(logowink0, 500);
+  render(logowink2, 500);
+  render(logowink0, 500);
+  render(logowink3, 500);
 }
+
 void ijhacklogo() {
   /* here is the data for the characters */
   byte ij[8]={
@@ -649,23 +502,10 @@ B11011110,
 B11000111
  };
 
-  bool skip;
-  skip = render(ij, 500);
-  if (skip) {
-    return;
-  }
-  skip = render(h, 500);
-  if (skip) {
-    return;
-  }
-  skip = render(a, 500);
-  if (skip) {
-    return;
-  }
-  skip = render(c, 500);
-  if (skip) {
-    return;
-  }
+  render(ij, 500);
+  render(h, 500);
+  render(a, 500);
+  render(c, 500);
   render(k, 500);
 }
 
@@ -692,10 +532,7 @@ B01111110,
 B00111100,
 B00011000};
 
-  bool skip = render(heart1, 500);
-  if (skip) {
-    return;
-  }
+  render(heart1, 500);
   render(heart2, 500); 
 }
 
@@ -720,10 +557,7 @@ B00100100,
 B01000010,
 B00000000};
 
-  bool skip = render(jumpdown, 500);
-  if (skip) {
-    return;
-  }
+  render(jumpdown, 500);
   render(jumpup, 500);  
 }
 
@@ -858,63 +692,21 @@ B00101000,
 B00011000,
 B00001000};
 
-  bool skip;
-  skip = render(heart6, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart6, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart7, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart8, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart9, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart10, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart11, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart12, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart13, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart1, 100);
-  if (skip) {
-    return;
-  }
-  skip = render(heart2, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart3, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart4, 100);  
-  if (skip) {
-    return;
-  }
-  skip = render(heart5, 100);
-  if (skip) {
-    return;
-  }
+  render(heart6, 100);  
+  render(heart6, 100);  
+  render(heart7, 100);  
+  render(heart8, 100);  
+  render(heart9, 100);  
+  render(heart10, 100);  
+  render(heart11, 100);  
+  render(heart12, 100);  
+  render(heart13, 100);  
+  render(heart1, 100);
+  render(heart2, 100);  
+  render(heart3, 100);  
+  render(heart4, 100);  
+  render(heart5, 100);
+  render(heart6, 100);  
   render(heart6, 100);  
 }
 
@@ -1069,63 +861,20 @@ B11111100,
 B11111111,
 B11111111};
 
-  bool skip;
-  skip = render(eq1, 50);
-  if (skip) {
-    return;
-  }
-  skip = render(eq2, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq3, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq4, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq5, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq6, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq7, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq8, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq9, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq10, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq11, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq12, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq13, 50);  
-  if (skip) {
-    return;
-  }
-  skip = render(eq14, 50);  
-  if (skip) {
-    return;
-  }
+  render(eq1, 50);
+  render(eq2, 50);  
+  render(eq3, 50);  
+  render(eq4, 50);  
+  render(eq5, 50);  
+  render(eq6, 50);  
+  render(eq7, 50);  
+  render(eq8, 50);  
+  render(eq9, 50);  
+  render(eq10, 50);  
+  render(eq11, 50);  
+  render(eq12, 50);  
+  render(eq13, 50);  
+  render(eq14, 50);  
   render(eq15, 50);     
 }
 
@@ -1306,95 +1055,28 @@ B00000001,
 B00000001,
 B00000001};
 
-  bool skip;
-  skip = render(invader1, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader2, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader3, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader4, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader5, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader6, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader7, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader8, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader9, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader9a, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader9, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader9a, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader9, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader9a, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader9, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader10, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader11, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader12, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader13, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader14, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader15, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(invader16, 200);
-  if (skip) {
-    return;
-  }
+  render(invader1, 200);
+  render(invader2, 200);
+  render(invader3, 200);
+  render(invader4, 200);
+  render(invader5, 200);
+  render(invader6, 200);
+  render(invader7, 200);
+  render(invader8, 200);
+  render(invader9, 200);
+  render(invader9a, 200);
+  render(invader9, 200);
+  render(invader9a, 200);
+  render(invader9, 200);
+  render(invader9a, 200);
+  render(invader9, 200);
+  render(invader10, 200);
+  render(invader11, 200);
+  render(invader12, 200);
+  render(invader13, 200);
+  render(invader14, 200);
+  render(invader15, 200);
+  render(invader16, 200);
   render(invader1, 200);
 }
 
@@ -1541,60 +1223,20 @@ B00000000,
 B00000000,
 B00011101};
 
-  bool skip;
-  skip = render(pong1, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong2, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong3, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong4, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong5, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong6, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(pong7, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong8, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong9, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong10, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong11, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong12, 200);
-  if (skip) {
-    return;
-  }
-  skip = render(pong13, 200);  
-  if (skip) {
-    return;
-  }
-  skip = render(pong14, 200);  
+    render(pong1, 200);  
+    render(pong2, 200);  
+    render(pong3, 200);  
+    render(pong4, 200);  
+    render(pong5, 200);  
+    render(pong6, 200);
+    render(pong7, 200);  
+    render(pong8, 200);  
+    render(pong9, 200);  
+    render(pong10, 200);  
+    render(pong11, 200);  
+    render(pong12, 200);
+    render(pong13, 200);  
+    render(pong14, 200);  
 }
 
 
@@ -1937,6 +1579,56 @@ B00000000};
   render(snake29, 200);
   render(snake30, 200);
 
+}
+
+
+
+void arrows() {
+  byte arrowup[8]={
+B00000000,
+B00011000,
+B00111100,
+B01111110,
+B00011000,
+B00011000,
+B00011000,
+B00000000};
+
+  byte arrowdown[8]={
+B00000000,
+B00011000,
+B00011000,
+B00011000,
+B01111110,
+B00111100,
+B00011000,
+B00000000};
+
+  byte arrowright[8]={
+B00000000,
+B00001000,
+B00001100,
+B01111110,
+B01111110,
+B00001100,
+B00001000,
+B00000000};
+
+
+  byte arrowleft[8]={
+B00000000,
+B00010000,
+B00110000,
+B01111110,
+B01111110,
+B00110000,
+B00010000,
+B00000000};
+
+  render(arrowup, 200);
+  render(arrowright, 200);  
+  render(arrowdown, 200);  
+  render(arrowleft, 200);  
 }
 
 
