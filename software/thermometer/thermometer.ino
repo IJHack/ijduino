68e98600a0e137b5dcff1d6d60392329f6ba05b0
Anne Jan Brouwer <ajbrouwer@totalactivemedia.nl>
2014-11-18 01:40:20 +0100

    cleanups

--- software/thermometer/thermometer.ino
+++ software/thermometer/thermometer.ino
@@ -35,20 +35,6 @@ void letter (const byte c)
   }
 }  // end of letter
 
-char *ftoa(char *a, double f, int precision)
-{
- long p[] = {0,10,100,1000,10000,100000,1000000,10000000,100000000};
- 
- char *ret = a;
- long heiltal = (long)f;
- itoa(heiltal, a, 10);
- while (*a != '\0') a++;
- *a++ = '.';
- long desimal = abs((long)((f - heiltal) * p[precision]));
- itoa(desimal, a, 10);
- return ret;
-}
-
 void showString (const char * s, const unsigned long time)
 {
   char c;
